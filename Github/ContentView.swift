e4e0fac5632790915691783c0625ded422b53de7
Henry Greenhut <henrysgreenhut@gmail.com>
2022-07-15 19:17:50 -0400

    Initial Commit

---
+++ Github/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Github
+//
+//  Created by Henry Greenhut on 7/15/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Github/GithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubApp.swift
+//  Github
+//
+//  Created by Henry Greenhut on 7/15/22.
+//
+
+import SwiftUI
+
+@main
+struct GithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
